3e79e6146bcdb2571e004feb27ebfd5c791bad77
rsmyers21 <rsmyers21@gmail.com>
2023-10-28 15:07:55 -0500

    Added the ability to edit and save tasks

--- script.js
+++ script.js
@@ -7,20 +7,48 @@ function createTask() {
     }
 
     else {
-       // let li = document.createElement("li");
-        //li.innerHTML = inputField.value;
-       // listContainer.appendChild(li);
-
         const taskElement = document.createElement("div");
         taskElement.classList.add("task")
 
         const taskContentElement = document.createElement("div");
         taskContentElement.classList.add("content");
-        taskContentElement.innerText = inputField.value;
 
         taskElement.appendChild(taskContentElement);
 
+        const taskInputElement = document.createElement("input");
+        taskInputElement.classList.add("text");
+        taskInputElement.type = "text";
+        taskInputElement.value = inputField.value;
+        taskInputElement.setAttribute("readonly", "readonly");
+
+        taskContentElement.appendChild(taskInputElement);
+
+        const taskEditElement = document.createElement("button");
+        taskEditElement.classList.add("edit")
+        taskEditElement.innerHTML = "Edit";
+
+        const taskDeleteElement = document.createElement("button");
+        taskDeleteElement.classList.add("delete");
+        taskDeleteElement.innerHTML = "Delete";
+
+        taskElement.appendChild(taskEditElement);
+        taskElement.appendChild(taskDeleteElement);
+
         listContainer.appendChild(taskElement);
+
+        inputField.value = "";
+
+        taskEditElement.addEventListener("click", () => {
+            if (taskEditElement.innerText.toLowerCase() == "edit") {
+            taskInputElement.removeAttribute("readonly");
+            taskInputElement.focus();
+            taskEditElement.innerText = "Save";
+            }
+            else {
+                taskInputElement.setAttribute("readonly", "readonly");
+                taskEditElement.innerText = "Edit";
+            }
+        } );
     }
 
 }
